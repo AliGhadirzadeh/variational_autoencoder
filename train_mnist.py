db29d390ef0014844c8c268fcd1a5d92b908f1e9
Ali <ali@Alis-MBP.lan>
2019-07-16 20:57:04 +0200

    the VAE model and train_mnist.py updated

--- train_mnist.py
+++ train_mnist.py
@@ -7,25 +7,71 @@ import vae
 import matplotlib.pyplot as plt
 import numpy as np
 
+import argparse
+
+parser = argparse.ArgumentParser(description='VAE training using MNIST dataset')
+parser.add_argument('--train', default=False,action='store_true', help='set it to train the model')
+parser.add_argument('--eval', default=False, action='store_true', help='set it  to evaluate the model')
+parser.add_argument('--path-to-model', default=None, type=str, help='the path to save/load the model')
+parser.add_argument('--model-filename', default=None, type=str, help='the filename of the model to be evaluated')
+parser.add_argument('--latent-size', default=5, type=int, help='the dimension of the latent variable')
+parser.add_argument('--image-size', default=12, type=int, help='MNIST images are resized by the --image-size')
+parser.add_argument('--show-sample-images', default=False, action='store_true', help='set it to show a number of sampled images from the dataset')
+parser.add_argument('--num-epoch', default=10000, type=int, help='the number of epochs to train the model')
+parser.add_argument('--snapshot', default=100, type=int, help='the number of epochs to make a snapshot of the training')
+parser.add_argument('--batch-size', default=100, type=int, help='the batch size')
+
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+if __name__ == '__main__':
+    args = parser.parse_args()
 
-trainset = torchvision.datasets.MNIST(root='./data', train=True,download=True,transform=transform)
-mnistloader = torch.utils.data.DataLoader(trainset, batch_size=100,shuffle=True, num_workers=0)
+    img_size = args.image_size
+    latent_size = args.latent_size
 
-#dataiter = iter(mnistloader)
-#img, label = dataiter.next()
-#imshow(torchvision.utils.make_grid(img))
+    # define the data transform
+    transform = transforms.Compose(
+        [transforms.Resize(img_size),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5,), (0.5,))])
 
-encoder = vae.FullyConnecteEncoder(28*28,5)
-decoder = vae.FullyConnecteDecoder(5,28*28)
-v_autoencoder = vae.VariationalAutoEncoder(mnistloader, encoder, decoder, device='cpu',n_epoch=10)
+    # construct the train and test data loader
+    size_test_data = 10000
 
-v_autoencoder.train()
+    trainset = torchvision.datasets.MNIST(root='./data', train=True,download=True,transform=transform)
+    testset = torchvision.datasets.MNIST(root='./data', train=False, transform=transform)
+
+    #trainset_part = torch.utils.data.random_split(trainset, [len(trainset)-size_test_data, size_test_data])[0]
+    testset_part = torch.utils.data.random_split(testset, [size_test_data, len(testset)-size_test_data])[0]
+
+    mnist_test_loader = torch.utils.data.DataLoader(testset_part, batch_size=args.batch_size, shuffle=True, num_workers=0)
+    mnist_train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,shuffle=True, num_workers=0)
+
+    # making the autoencoder
+    encoder = vae.FullyConnecteEncoder(img_size*img_size,latent_size)
+    decoder = vae.FullyConnecteDecoder(latent_size,img_size*img_size)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print('training using device: {}'.format(device))
+
+    v_autoencoder = vae.VariationalAutoEncoder(encoder, decoder,path_to_model=args.path_to_model, device=device,n_epoch=args.num_epoch)
+    v_autoencoder.snapshot = args.snapshot
+
+    # show some sample images
+    if args.show_sample_images:
+        dataiter = iter(mnist_train_loader)
+        img, label = dataiter.next()
+        imshow(torchvision.utils.make_grid(img))
+
+    # train the model
+    if args.train:
+        v_autoencoder.train_vae(mnist_train_loader)
+
+    # evaluate the model
+    if args.eval:
+        if not args.train:
+            v_autoencoder.load_model(args.model_filename)
+        v_autoencoder.evaluate(mnist_test_loader)
--- vae.py
+++ vae.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 class FullyConnecteEncoder(nn.Module):
     def __init__(self, input_size, output_size):
@@ -57,7 +60,7 @@ class FullyConnecteDecoder(nn.Module):
 
 
 class VariationalAutoEncoder(nn.Module):
-    def __init__(self, data_loader, encoder, decoder, device='cpu', n_epoch=10000,
+    def __init__(self, encoder, decoder, path_to_model='vae_model/', device='cpu', n_epoch=10000,
                  beta_interval=10, beta_min=1.0e-6, beta_max=1.0e-0, lr = 1e-3):
 
         super(VariationalAutoEncoder, self).__init__()
@@ -69,16 +72,20 @@ class VariationalAutoEncoder(nn.Module):
         self.decoder = decoder.to(self.device)
 
         self.optimizer = optim.Adam(self.parameters(), self.lr)
-        self.data_loader = data_loader
 
         self.deploy = False
 
+        self.model_dir=path_to_model
+
+        self.epoch = 0
         self.n_epoch = n_epoch
         self.beta = beta_min
         self.beta_min = beta_min
         self.beta_max = beta_max
         self.beta_interval = beta_interval
         self.beta_idx = 0
+
+        self.snapshot = 100 # number of epoch
 
     def forward(self, x):
         mu, logstd = self.encoder(x)
@@ -92,40 +99,95 @@ class VariationalAutoEncoder(nn.Module):
         xhat = xhat.view(x.size())
         return xhat, mu, logstd
 
-    def loss(self, x):
-        xhat,  mu, logsd = self.forward(x)
+    def loss(self,x,xhat,mu,logsd):
         renonstruction_loss = F.mse_loss(xhat, x)
         var = (logsd.exp()) ** 2
         kld = 0.5 * torch.sum(-2*logsd + mu ** 2 + var , 1) - 0.5
         kld_loss = kld.mean()
         return renonstruction_loss + self.beta * kld_loss, renonstruction_loss, kld_loss
 
-    def update_beta(self, epoch):
+    def update_beta(self):
         epoch_to_update = (self.beta_idx+1.0)/self.beta_interval*self.n_epoch
-        if epoch > epoch_to_update:
+        if self.epoch > epoch_to_update:
             self.beta = (self.beta_idx+1.0)/self.beta_interval*(self.beta_max-self.beta_min)
             self.beta_idx += 1
             print ("beta updated - new value: ", self.beta)
 
-    def train(self):
-        for epoch in range(self.n_epoch):
-            self.update_beta(epoch)
+    def train_vae(self, data_loader):
+        self.train()
+        self.deploy = False
+
+        self.hist_losses = np.zeros((self.n_epoch,3))
+        for self.epoch in range(self.n_epoch):
+            self.update_beta()
             sum_loss = 0.0
             sum_reconst_loss = 0.0
             sum_kdl_loss = 0.0
-            for x in self.data_loader:
+            for x in data_loader:
                 if isinstance(x, list):
-                    x = x[0]
+                    x = x[0].to(self.device)
                 self.optimizer.zero_grad()
-                loss, reconst_loss, kdl_loss = self.loss(x.to(self.device))
+                xhat,  mu, logsd = self.forward(x)
+                loss, reconst_loss, kdl_loss = self.loss(x, xhat, mu, logsd)
                 loss.backward()
                 self.optimizer.step()
                 sum_loss += loss.item()
                 sum_reconst_loss += reconst_loss.item()
                 sum_kdl_loss += kdl_loss.item()
 
-            print('[%d] loss: %.6e' %(epoch + 1, sum_loss / len(self.data_loader)))
-            print('\treconst_loss: %.6e' %(sum_reconst_loss / len(self.data_loader)))
-            print('\tkdl_loss: %.6e' %(sum_kdl_loss / len(self.data_loader)))
+            print('[%d] loss: %.6e' %(self.epoch + 1, sum_loss / len(data_loader)))
+            print('\treconst_loss: %.6e' %(sum_reconst_loss / len(data_loader)))
+            print('\tkdl_loss: %.6e' %(sum_kdl_loss / len(data_loader)))
+            self.hist_losses[self.epoch] = np.array([sum_loss, sum_reconst_loss, sum_kdl_loss])/ len(data_loader)
 
-        #torch.save(self.model.state_dict(), 'model.mdl')
+            if self.epoch % self.snapshot == (self.snapshot-1) or self.epoch == (self.n_epoch-1):
+                self.save_model()
+
+
+    def evaluate(self, data_loader):
+        self.eval()
+        self.deploy = True
+        sum_loss = 0.0
+        sum_reconst_loss = 0.0
+        sum_kdl_loss = 0.0
+
+        batch_size = data_loader.batch_size
+        latent_var = np.zeros((len(data_loader)*batch_size, self.latent_size))
+
+        for i, x in enumerate(data_loader):
+            if isinstance(x, list):
+                x = x[0].to(self.device)
+            xhat,  mu, logsd = self.forward(x)
+            _, reconst_loss, kdl_loss = self.loss(x, xhat, mu, logsd)
+            sum_reconst_loss += reconst_loss.item()
+            sum_kdl_loss += kdl_loss.item()
+            lv = np.copy(mu.detach().numpy())
+            c_batch_size = lv.shape[0]
+            latent_var[i*batch_size:i*batch_size+c_batch_size,:] = lv
+
+        print('reconst_loss: %.6e' %(sum_reconst_loss / len(data_loader)))
+        print('kdl_loss: %.6e' %(sum_kdl_loss / len(data_loader)))
+
+        for i in range(self.latent_size):
+            plt.subplot(510+i+1)
+            plt.hist(latent_var[:,i], bins=20)
+            plt.xlim((-1,1))
+        plt.show()
+
+    def save_model(self):
+        filepath = os.path.join(self.model_dir, 'vae_{:04d}.mdl'.format(self.epoch))
+        torch.save(self.state_dict(), filepath)
+        plt_labels = ['loss', 'reconst. loss', 'kdl loss']
+        for i in range(3):
+            plt.plot(np.arange(self.epoch),self.hist_losses[:self.epoch,i], label=plt_labels[i])
+        plt.xlabel('epoch')
+        plt.ylabel('losses')
+        plt.legend(loc='upper right')
+        filepath = os.path.join(self.model_dir, 'loss_{:04d}.jpg'.format(self.epoch))
+        plt.savefig(filepath)
+        plt.clf()
+
+    def load_model(self, filename):
+        filepath = os.path.join(self.model_dir, filename)
+        self.load_state_dict(torch.load(filepath) )
+        self.eval()
