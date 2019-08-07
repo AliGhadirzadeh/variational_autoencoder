3b87613d260ab90253812eeac87e75862682baf5
sebastianghomri <sebastian.ghomri@gmail.com>
2019-08-07 13:35:29 +0200

    Adding arguments to snippet script

--- data_loading_scripts/snippets_script.py
+++ data_loading_scripts/snippets_script.py
@@ -1,29 +1,44 @@
 import numpy as np
 import queue as queue
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Creates snippets to train VAE given EEG data and time discontinuity data')
+parser.add_argument('--snippet_length', default=500, help='Length of snippets')
+parser.add_argument('--window_length', default=100, help='Length of window of overlap')
+args = parser.parse_args()
+
 # Initialize parameters and subjects
-snippet_length = 500
-window = 100
+snippet_length = int(args.snippet_length)
+window_length = int(args.window_length)
 subject_list = []
-for i in range(52):
+for i in range(1, 52):
 	subject_list.append(str(i))
 
 for subject in subject_list:
+	print(subject)
 	# Load data, create queue
 	# eeg_data is numpy array containing channel voltages over time
 	# disc_data is a numpy array containing the times (indices) of discontinuities
-	eeg_path = "/home/sebgho/eeg_project/data/" + subject + ".npy"
-	disc_path = "/home/sebgho/eeg_project/data/" + subject + "_disc.npy"
-	eeg_data = np.load(eeg_path)
-	disc_data = np.load(disc_path)
+	eeg_path = "/home/sebgho/eeg_project/data/subjects/npy/" + subject + ".npy"
+	disc_path = "/home/sebgho/eeg_project/data/subjects/npy/" + subject + "_disc.npy"
+	try:
+		eeg_data = np.load(eeg_path)
+	except:
+		continue
+	try:
+		disc_data = np.load(disc_path)
+	except:
+		continue
 
-	disc_queue = queue.Queue()
-	for i in range(disc_data.shape[0]):
-		disc_queue.put(disc_data[i])
-
-	# Initialize times and snippet list
+	# Initialize times, queue and snippet list
 	t = 0
 	t_max = eeg_data.shape[0]
+	disc_queue = queue.Queue()
+	if disc_data.shape[0] == 0:
+		disc_queue.put(t_max)
+	for i in range(disc_data.shape[0]):
+		disc_queue.put(disc_data[i])
 	t_disc = disc_queue.get()
 	snippet_list = []
 
@@ -32,7 +47,7 @@ for subject in subject_list:
 		# If no discontinuity, add snippet
 		if t + snippet_length < t_disc:
 			snippet_list.append(eeg_data[t:(t+snippet_length), :])
-			t += window
+			t += window_length
 		# If dicontinuity, jump to after discontinuity
 		else:
 			t = t_disc + 1
@@ -51,4 +66,4 @@ for subject in subject_list:
 							  number_of_channels))
 	for snippet_index in range(number_of_snippets):
 		snippet_array[snippet_index] = snippet_list[snippet_index]
-	np.save("/home/sebgho/eeg_project/data/" + subject + "_snippets.npy", snippet_array)
+	np.save("/home/sebgho/eeg_project/data/subjects/npy/" + subject + "_snippets.npy", snippet_array)
