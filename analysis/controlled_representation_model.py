5d195642b54ad4276d14358f2bde9f922f47a403
Sebastian Ghomri <sebastian.ghomri@gmail.com>
2021-10-28 23:29:10 +0200

    bug fixes, working

--- analysis/controlled_representation_model.py
+++ analysis/controlled_representation_model.py
@@ -14,23 +14,21 @@ from sklearn.metrics import r2_score, accuracy_score
 
 class Dataset(Dataset):
 
-    def __init__(self, x, df, target_string=None):
+    def __init__(self, x, df):
         self.x = x
         self.df = df
         self.dep_variable = None
         self.indep_variable = None
-        self.ctrl_variables = []
+        self.ctrl_variable = None
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, index):
-        dep_sample = self.x[index]
-        indep_sample = torch.as_tensor(self.df[self.indep_variable][index])
-        ctrl_sample = {}
-        for var in self.ctrl_variables:
-            ctrl_sample[var] = torch.as_tensor(self.df[var][index])
-        return dep_sample, indep_sample, ctrl_sample
+        indep_sample = self.x[index]
+        dep_sample = torch.as_tensor(self.df[self.dep_variable][index])
+        ctrl_sample = torch.as_tensor(self.df[self.ctrl_variable][index])
+        return indep_sample, dep_sample, ctrl_sample
 
     def train_test_split(self, test_size=0.2):
         train_size = int((1-test_size) * len(self))
@@ -47,8 +45,7 @@ class PredictorNetwork(nn.Module):
     def construct_network(self, x, y):
         self.set_type(y)
         num_inputs = x.shape[1]
-        num_channels = 50
-        num_outputs = None
+        num_channels = [200, 100, 50]
 
         if self.type == "categorical":
             self.criterion = nn.CrossEntropyLoss()
@@ -59,9 +56,12 @@ class PredictorNetwork(nn.Module):
         else:
             raise
 
-        layers = [nn.Linear(num_inputs, num_channels), nn.ReLU()]
-        layers += [nn.Linear(num_channels, num_outputs)]
-        
+        layers = []
+        for i, channels in enumerate(num_channels):
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [nn.Linear(in_channels, out_channels), nn.ReLU()]
+        layers += [nn.Linear(num_channels[-1], num_outputs)]
         self.network = nn.Sequential(*layers)
 
     def set_type(self, y):
@@ -71,7 +71,6 @@ class PredictorNetwork(nn.Module):
             self.type = "continuous"
         else:
             raise
-
 
     def forward(self, x):
         return self.network(x)
@@ -98,20 +97,36 @@ class ExtractorNetwork(nn.Module):
         super(ExtractorNetwork, self).__init__()
         self.network = None
         self.criterion = nn.MSELoss()
+        self.conv = False
 
     def construct_network(self, x):
-        num_inputs = x.shape[1]
-        num_channels = 50
-        num_outputs = 50
-        layers = [nn.Linear(num_inputs, num_channels), nn.ReLU()]
-        layers += [nn.Linear(num_channels, num_outputs), nn.ReLU()]
-        self.network = nn.Sequential(*layers)
+        if self.conv:
+            num_inputs = x.shape[1]
+            conv_channels = [60, 40, 20]
+            conv_kers = [10, 5, 2]
+            layers = []
+            for i in range(len(conv_channels)):
+                in_channels = num_inputs if i == 0 else conv_channels[i-1]
+                out_channels = conv_channels[i]
+                ker_size = conv_kers[i]
+                conv = nn.Conv1d(in_channels, out_channels, ker_size)
+                layers += [conv, nn.ReLU(), nn.MaxPool1d(2)]
+            layers += [nn.Flatten()]
+            self.network = nn.Sequential(*layers)
+        else:
+            num_inputs = x.shape[1]
+            num_channels = [100, 100, 100]
+            num_outputs = 100
+            layers = []
+            for i in range(len(num_channels)):
+                in_channels = num_inputs if i == 0 else num_channels[i-1]
+                out_channels = num_channels[i]
+                layers += [nn.Linear(in_channels, out_channels), nn.ReLU()]
+            layers += [nn.Linear(num_channels[-1], num_outputs)]
+            self.network = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.network(x)
-
-    def loss(self, x_hat, x):
-        return self.criterion(x_hat, x)
 
 
 class ControlledNetwork(nn.Module):
@@ -130,122 +145,147 @@ class ControlledNetwork(nn.Module):
 
             self.dep_predictor = PredictorNetwork()
             self.dep_predictor.construct_network(x_latent, y)
-
-
-            self.ctrl_predictors = {}
-            for var in c.keys():
-                self.ctrl_predictors[var] = PredictorNetwork()
-                self.ctrl_predictors[var].construct_network(x_latent, c[var])
+            
+            self.ctrl_predictor = PredictorNetwork()
+            self.ctrl_predictor.construct_network(x_latent, c)
 
     def forward(self, x):
         latent_x = self.extractor(x)
         y = self.dep_predictor(latent_x)
-        c = {}
-        for var in self.ctrl_predictors.keys():
-            c[var] = self.ctrl_predictors[var](latent_x)
+        c = self.ctrl_predictor(latent_x)
         return y, c
 
     def learn(self, data):
         train_data, val_data = data.train_test_split()
         train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
         val_loader = DataLoader(val_data, batch_size=32, shuffle=True)
-        opt = optim.Adam(self.parameters(), lr=3e-4)
+
+        # One optimizer per network
+        extractor_opt = optim.Adam(self.parameters(), lr=3e-4)
+        dep_predictor_opt = optim.Adam(self.parameters(), lr=3e-4)
+        ctrl_predictor_opt = optim.Adam(self.parameters(), lr=3e-4)
+
         for epoch in tqdm(range(self.n_epoch)):
+
+            # TRAIN MODEL ON TRAIN DATA
             for b_x, b_y, b_c in train_loader:
                 self.train()
-                opt.zero_grad()
+
+                # ---- TRAIN EXTRACTOR ----
+                # Activate extractor, de-activate rest, zero_grad
+                self.deactivate(self.dep_predictor)
+                self.deactivate(self.ctrl_predictor)
+                self.activate(self.extractor)
+                extractor_opt.zero_grad()
+                # Forward pass, calculate loss, backward pass
                 b_y_hat, b_c_hat = self.forward(b_x)
-                loss_tuple = self.loss(b_y_hat, b_y, b_c_hat, b_c)
-                self.update(loss_tuple)
-                opt.step()
+                extractor_loss = self.extractor_loss(b_y_hat, b_y, b_c_hat, b_c)
+                extractor_loss.backward()
+                extractor_opt.step()
+
+                # ---- TRAIN DEP_PREDICTOR ----
+                # Activate dep_predictor, de-activate rest, zero_grad
+                self.deactivate(self.extractor)
+                self.deactivate(self.ctrl_predictor)
+                self.activate(self.dep_predictor)
+                dep_predictor_opt.zero_grad()
+                # Forward pass, calculate loss, backward pass
+                b_y_hat, b_c_hat = self.forward(b_x)
+                dep_predictor_loss = self.dep_predictor.loss(b_y_hat, b_y)
+                dep_predictor_loss.backward()
+                dep_predictor_opt.step()
+
+                # ---- TRAIN CTRL_PREDICTORS ----
+                # Activate each ctrl_predictor, de-activate rest, zero_grad
+                self.deactivate(self.extractor)
+                self.deactivate(self.dep_predictor)
+                self.activate(self.ctrl_predictor)
+                # Forward pass, calculate loss, backward pass
+                ctrl_predictor_opt.zero_grad()
+                b_y_hat, b_c_hat = self.forward(b_x)
+                ctrl_predictor_loss = self.ctrl_predictor.loss(b_c_hat, b_c)
+                ctrl_predictor_loss.backward()
+                ctrl_predictor_opt.step()
+            
+            # EVALUATE MODEL ON VALIDATION DATA
             with torch.no_grad():
-                iterations = 0
-                extractor_loss = 0
-                dep_loss = 0
-                ctrl_loss = 0
+                self.eval()
+                self.print_stats(val_loader)
 
-                dep_score = 0
-                ctrl_score = 0
-                for b_x, b_y, b_c in val_loader:
-                    b_y_hat, b_c_hat = self.forward(b_x)
-                    loss_tuple = self.loss(b_y_hat, b_y, b_c_hat, b_c)
-                    extractor_loss += loss_tuple[0]
-                    dep_loss += loss_tuple[1]
-                    ctrl_loss += loss_tuple[2]["target"]
+    def activate(self, network):
+        for param in network.parameters():
+            param.requires_grad = True
 
-                    score_tuple = self.score(b_y_hat, b_y, b_c_hat, b_c)
-                    dep_score += score_tuple[0]
-                    ctrl_score += score_tuple[1]["target"]
-
-                    iterations += 1
-
-                extractor_loss /= iterations
-                dep_loss /= iterations
-                ctrl_loss /= iterations
-
-                dep_score /= iterations
-                ctrl_score /= iterations
-
-                print("Extractor loss (validation): %.4f" % extractor_loss)
-                print("Dependent variable loss (validation): %.4f" % dep_loss)
-                print("Control variable loss (validation): %.4f" % ctrl_loss)
-                print("Dependent variable score (validation): %.4f" % dep_score)
-                print("Control variable score (validation): %.4f" % ctrl_score)
-
-    def update(self, loss_tuple):
-        extractor_loss, dep_loss, ctrl_loss = loss_tuple
-        extractor_loss.backward(retain_graph=True)
-        dep_loss.backward(retain_graph=True)
-        for var, var_loss in ctrl_loss.items():
-            var_loss.backward(retain_graph=True)
-
-    def loss(self, y_hat, y, c_hat, c):
-        # Dependent variable predictor trained to minimize loss
+    def deactivate(self, network):
+        for param in network.parameters():
+            param.requires_grad = False
+                
+    def extractor_loss(self, y_hat, y, c_hat, c):
         dep_loss = self.dep_predictor.loss(y_hat, y)
-        # Control variable predictors trained to minimize individual loss
-        ctrl_loss = {}
-        ctrl_loss_sum = 0
-        for var in c.keys():
-            ctrl_loss[var] = self.ctrl_predictors[var].loss(c_hat[var], c[var])
-            ctrl_loss_sum += ctrl_loss[var]
+        ctrl_loss = self.ctrl_predictor.loss(c_hat, c)
 
         # Extractor trained to minimize composite loss
-        extractor_loss = dep_loss - 5*ctrl_loss_sum
-        return extractor_loss, dep_loss, ctrl_loss
+        extractor_loss = dep_loss - 0.5 * ctrl_loss
+        return extractor_loss
+    
+    def print_stats(self, test_loader):
+        n_batches = len(test_loader)
+        i = 0
+        extractor_loss = np.zeros(n_batches)
+        dep_loss = np.zeros(n_batches)
+        ctrl_loss = np.zeros(n_batches)
 
-    def score(self, y_hat, y, c_hat, c):
-        # Dependent variable predictor trained to minimize loss
-        dep_score = self.dep_predictor.score(y_hat, y)
-        # Control variable predictors trained to minimize individual loss
-        ctrl_score = {}
-        for var in c.keys():
-            ctrl_score[var] = self.ctrl_predictors[var].score(c_hat[var], c[var])
-        return dep_score, ctrl_score
+        dep_score = np.zeros(n_batches)
+        ctrl_score = np.zeros(n_batches)
+
+        for b_x, b_y, b_c in test_loader:
+            b_y_hat, b_c_hat = self.forward(b_x)
+
+            extractor_loss[i] = self.extractor_loss(b_y_hat, b_y, b_c_hat, b_c)
+            dep_loss[i] = self.dep_predictor.loss(b_y_hat, b_y)
+            ctrl_loss[i] += self.ctrl_predictor.loss(b_c_hat, b_c)
+
+            dep_score[i] = self.dep_predictor.score(b_y_hat, b_y)
+            ctrl_score[i] = self.ctrl_predictor.score(b_c_hat, b_c)
+
+            i += 1
+        print("Extractor loss (validation): %.4f" % extractor_loss.mean())
+        print("Dependent variable loss (validation): %.4f" % dep_loss.mean())
+        print("Control variable loss (validation): %.4f" % ctrl_loss.mean())
+
+        print("Dependent variable score (validation): %.4f" % dep_score.mean())
+        print("Control variable score (validation): %.4f" % ctrl_score.mean())
 
 
 
+
+# EEG
 """
-x, y = utils.get_data("EEG_flat", "subject_id")
+x, y = utils.get_data("EEG", "subject_id")
 x = torch.Tensor(x)
 y = torch.Tensor(y)
 df = pd.read_pickle("../../data/data/numpy_files/df.pkl")
 
 data = Dataset(x, df)
-data.dep_variable = "EEG"
-data.indep_variable = "math_t1"
-data.ctrl_variables += ["subject_id"]
+data.indep_variable = "EEG"
+data.dep_variable = "math_t1"
+data.ctrl_variable = "subject_id"
 """
+
+# Digits
 
 x, y = utils.get_data("digits", "subject_id")
 x = torch.Tensor(x)
 df = pd.DataFrame(y, columns=["target"])
 
 data = Dataset(x, df)
-data.dep_variable = "digits"
-data.indep_variable = "target"
-data.ctrl_variables += ["target"]
+data.dep_variable = "target"
+data.indep_variable = "digits"
+data.ctrl_variable = "target"
 
 
 cn = ControlledNetwork()
 cn.construct_network(data)
+#print(cn.dep_predictor.type)
+#print(cn.ctrl_predictors[data.ctrl_variables[0]].type)
 cn.learn(data)
